7ab3f82a79099483718eaab8b1bcf33eb1774a82
SafarSoFar <cafar0525@gmail.com>
2024-07-23 16:14:18 +0500

    added simple star movement

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::{ops::{Add, Div, Sub, SubAssign}, thread::sleep, time::Duration};
 use raylib::prelude::*;
 use rand::prelude::*;
 
 const WINDOW_WIDTH : i32 = 640;
 const WINDOW_HEIGHT : i32 = 480;
 
+const STARS_AMOUNT : u32 = 100;
 
 
 fn main() {
@@ -11,19 +13,51 @@ fn main() {
         .size(WINDOW_WIDTH, WINDOW_HEIGHT)
         .title("Starfield")
         .build();
-    
-    let x = rl.get_random_value::<i32>(0..WINDOW_WIDTH);
-    let y = rl.get_random_value::<i32>(0..WINDOW_HEIGHT);
+    rl.set_target_fps(60);
+    let mut stars : Vec<Star> = Vec::new();
+    for _ in 0..STARS_AMOUNT{
+        stars.push(Star::new());
+    }
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
-        d.draw_circle(x,y, 5.0, Color::BLACK);
+
+        for star in stars.iter_mut(){
+            star.change_pos();
+            d.draw_circle(star.x_cur, star.y_cur, 5., Color::BLACK);
+        }
+
         d.clear_background(Color::WHITE);
-        //d.draw_text("Hello, world!", 12, 12, 20, Color::BLACK);
     }
 }
 
 struct Star{
-    x : u32,
-    y : u32,
-    z : i32,
+    x_init : i32,
+    y_init : i32,
+    z_init : i32,
+    x_cur : i32,
+    y_cur : i32,
+}
+
+impl Star{
+    pub fn new() -> Self{
+        Self {
+            x_init : thread_rng().gen_range(0..WINDOW_WIDTH), 
+            y_init: thread_rng().gen_range(0..WINDOW_WIDTH), 
+            z_init: WINDOW_WIDTH, 
+            x_cur: 0,
+            y_cur : 0,
+        }
+    }
+
+    pub fn change_pos(&mut self){
+        self.z_init -= 1;
+        //self.x -= 1;
+        //self.y -= 1;
+        self.x_cur = map_star_speed(self.x_init as f32 / self.z_init as f32, WINDOW_WIDTH as f32);
+        self.y_cur = map_star_speed(self.y_init as f32 / self.z_init as f32, WINDOW_HEIGHT as f32);
+    }
+}
+
+pub fn map_star_speed(star_screen_ratio : f32, boundary : f32) -> i32{
+    (star_screen_ratio * boundary) as i32
 }
