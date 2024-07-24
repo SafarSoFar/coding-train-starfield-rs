805f1d66aadaed8c7084f9bf27252acd3cc54a8b
SafarSoFar <cafar0525@gmail.com>
2024-07-24 10:57:32 +0500

    added overall color change, speed change depending on mouse x/y positions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::{ops::{Add, Div, Sub, SubAssign}, thread::sleep, time::Duration};
 use raylib::prelude::*;
 use rand::prelude::*;
 
@@ -6,11 +5,12 @@ const WINDOW_WIDTH : i32 = 640;
 const WINDOW_HEIGHT : i32 = 480;
 const WINDOW_CENTER : Vector2 = Vector2::new((WINDOW_WIDTH/2) as f32, (WINDOW_HEIGHT/2) as f32);
 const STARS_AMOUNT : u32 = 800;
-const STAR_MAX_SIZE : f32 = 12.;
-const SIMULATION_SPEED : i32 = 50;
+const STAR_MAX_SIZE : f32 = 3.;
+const MAX_SIMULATION_SPEED : f32 = 50.;
 
 
 fn main() {
+
     let (mut rl, thread) = raylib::init()
         .size(WINDOW_WIDTH, WINDOW_HEIGHT)
         .title("Starfield")
@@ -19,14 +19,36 @@ fn main() {
 
     let camera2D = Camera2D{target: Vector2::zero(), offset: WINDOW_CENTER, rotation : 0., zoom : 1.};
 
+    let mut background_color : Color = Color{r:255, g: 255, b: 255, a: 255};
+    let mut star_color : Color = Color{r : 0, g : 0, b : 0, a : 255};
+    let mut simulation_speed : i32 = 10;
+
     let mut stars : Vec<Star> = Vec::new();
     for _ in 0..STARS_AMOUNT{
         stars.push(Star::new());
     }
     while !rl.window_should_close() {
+
+
+        let mouse_pos_vec = rl.get_mouse_position();
+
+        // Calculating the simulation speed depending on mouse X position within the window frame, 
+        // where higher mouse x coordinates give faster speed 
+        simulation_speed = (mouse_pos_vec.x / WINDOW_WIDTH as f32 * MAX_SIMULATION_SPEED) as i32;
+
+
+        // Calculating the background color rgb channels value depending on mouse Y position within the window frame,
+        // where higher mouse Y coordinates give brighter overall color 
+        let bg_color_channels : u8 =  (mouse_pos_vec.y as f32 / WINDOW_HEIGHT as f32 * 255.) as u8;
+        background_color = Color{r: bg_color_channels, g : bg_color_channels, b : bg_color_channels, a : 255};
+
+        // Inverting background rgb channels value for stars in order to create a contrast and to be able to see stars
+        star_color = Color{r: 255 - bg_color_channels, g : 255 - bg_color_channels, b : 255 - bg_color_channels, a : 255};
+
         let mut d = rl.begin_drawing(&thread);
 
-        d.clear_background(Color::WHITE);
+
+        d.clear_background(background_color);
         {
 
             // Starting in 2d camera mode in order to shift
@@ -34,9 +56,9 @@ fn main() {
             let mut d = d.begin_mode2D(camera2D);
 
             for star in stars.iter_mut(){
-                star.change_pos();
-                //d.draw_circle(star.x_speed, star.y_speed, star.radius, Color::BLACK);
-                d.draw_line(star.x_prev, star.y_prev, star.x_speed, star.y_speed, Color::BLACK);
+                star.update(simulation_speed);
+                d.draw_circle(star.x_speed, star.y_speed, star.radius, star_color);
+                d.draw_line(star.x_prev, star.y_prev, star.x_speed, star.y_speed, star_color);
             }
 
         }
@@ -81,8 +103,8 @@ impl Star{
         }
     }
 
-    pub fn change_pos(&mut self){
-        self.z -= SIMULATION_SPEED;
+    pub fn update(&mut self, simulation_speed : i32){
+        self.z -= simulation_speed;
         if self.z < 1{
             self.z = WINDOW_WIDTH;
             self.x = thread_rng().gen_range(-WINDOW_WIDTH..WINDOW_WIDTH);
